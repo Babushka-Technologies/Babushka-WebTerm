2b5b60455e3290fbfc93cd768bb65d9d581057d9
lax1dude <92958986+nhOCTO@users.noreply.github.com>
2022-12-06 14:04:17 -0500

    Update contributors.js

--- js/contributors.js
+++ js/contributors.js
@@ -1,12 +1,10 @@
 const contributors = [
     {
-        name: 'jcubic',
-        fullname: 'Jakub Jankiewicz',
-        url: 'https://jcubic.pl/me'
+        name: 'nhOCTO',
+        fullname: 'Nicholas Harris',
     },
     {
-        fullname: 'Brett Zamir',
-        name: 'brettz9',
-        url: 'http://brett-zamir.me/'
+        fullname: 'Aiden McGarigle',
+        name: 'CesarPKhan',
     }
 ];
